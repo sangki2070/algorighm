cf3a30fa239bbe307066312958cf42086403d72b
sangki2070 <devsangki@gmail.com>
2021-11-30 15:24:24 +0900

    1130 algorighm

--- programmers/1130.js
+++ programmers/1130.js
@@ -1,45 +1,66 @@
-// 2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
+// 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
 
-// 입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
+// 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+// 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+// 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+// 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 
 // 제한 조건
-// 2016년은 윤년입니다.
-// 2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+// 시험은 최대 10,000 문제로 구성되어있습니다.
+// 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+// 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
 
-// ********** 풀이 ****************
 
-const week = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+// ************ 풀이 ***************
 
-let month = {
-    1 : 31,
-    2 : 29,
-    3 : 31,
-    4 : 30,
-    5 : 31,
-    6 : 30,
-    7 : 31,
-    8 : 31,
-    9 : 30,
-    10 : 31,
-    11 : 30,
-    12 : 31
+function solution(answers) {
+    let answer = []
+    
+    let a = [1,2,3,4,5]
+    let b = [2,1,2,3,2,4,2,5]
+    let c = [3,3,1,1,2,2,4,4,5,5]
+    
+    
+    let result = 
+        answers.filter((el,index) => el ===a[index % a.length]).length
+    let result2 =
+        answers.filter((el,index) => el === b[index % b.length]).length
+    let result3 = 
+        answers.filter((el,index) => el === c[index % c.length]).length
+    
+    let max = Math.max(result, result2, result3)
+    
+    if(result === max){
+        answer.push(1)
+    }
+    if(result2 === max){
+        answer.push(2)
+    }
+    if(result3 === max){
+        answer.push(3)
+    }
+    return answer
 }
 
-// [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-function solution(a, b) {
-    
+// ===========================================
+
+// 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+// ************* 풀이 ************************
+
+function solution(numbers) {
+    let answer = []
     let sum = 0
-    let sum2 = 0
-    let answer = ""
     
-    for(let i = 1; i < a ; i ++){
-            sum = sum + month[i]
+    for(let i = 0; i < numbers.length; i ++){
+        for(let j = i + 1; j< numbers.length; j++){
+            sum = numbers[i] + numbers[j]
+            if(!answer.includes(sum)){
+                answer.push(sum)
+            }
+        }
     }
     
-    sum2 = (sum + b -1) % 7
-    
-    answer = week[sum2]
-    
-    return answer
- 
+    return answer.sort((a,b) => a-b)
 }
