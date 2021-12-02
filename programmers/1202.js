3726e93a38db96c3d6c2a3550b708b26408dd78d
sangki2070 <devsangki@gmail.com>
2021-12-02 17:46:50 +0900

    1202 algorithm 3

--- programmers/1202.js
+++ programmers/1202.js
@@ -288,3 +288,42 @@ function solution(a, b) {
     }
     return answer
 }
+
+//===========================================
+
+// 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+
+// *************** 풀이 *********************
+
+function solution(n) {
+    let answer = 0
+    let arr = []
+    let result = n.toString(3).split("")
+    
+    for(let i = 0; i < result.length; i ++){
+        arr.unshift(result[i])
+    }
+    answer = parseInt(arr.join(""),3)
+    return answer
+}
+
+
+//===========================================
+
+// 어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+
+// *************** 풀이 *********************
+
+function solution(absolutes, signs) {
+    let answer = 0
+    
+    for(let i =0; i < absolutes.length; i ++){
+        if(signs[i]){
+            answer = answer + absolutes[i]
+        } else {
+            answer = answer - absolutes[i]
+        }
+    }
+    return answer
+}
