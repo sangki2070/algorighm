06658face0509de1ab7563405f2e8630ec0f125d
sangki2070 <devsangki@gmail.com>
2022-01-20 22:26:05 +0900

    1020 algorithm 4

--- leetcode_easy/0120.js
+++ leetcode_easy/0120.js
@@ -61,3 +61,28 @@ var buildArray = function(nums) {
     }
     return result
 };
+
+// **************************************
+
+
+// There is a programming language with only four operations and one variable X:
+
+// ++X and X++ increments the value of the variable X by 1.
+// --X and X-- decrements the value of the variable X by 1.
+// Initially, the value of X is 0.
+
+// Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+// ================= 풀이 ================
+
+var finalValueAfterOperations = function(operations) {
+    let answer = 0
+    for(let i = 0; i < operations.length; i++){
+        if(operations[i] === "++X" || operations[i] === "X++"){
+            answer += 1
+        } else {
+            answer -= 1
+        }
+    }
+    return answer
+};
