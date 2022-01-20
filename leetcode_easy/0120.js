73708a65bf6bf672d6248dad8a15c320028491f6
sangki2070 <devsangki@gmail.com>
2022-01-20 22:18:04 +0900

    1020 algorithm 3

--- leetcode_easy/0120.js
+++ leetcode_easy/0120.js
@@ -44,3 +44,20 @@ var isValid = function(s) {
 var getConcatenation = function(nums) {
     return [...nums, ...nums]
 };
+
+// **************************************
+
+
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+// ================= 풀이 ================
+
+var buildArray = function(nums) {
+    let result = []
+    for(let i = 0; i < nums.length; i ++){
+        result.push(nums[nums[i]])
+    }
+    return result
+};
