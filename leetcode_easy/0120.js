99cf32bfa54f24f278799b98955a019014937fb1
sangki2070 <devsangki@gmail.com>
2022-01-20 22:12:38 +0900

    1020 algorithm 2

--- leetcode_easy/0120.js
+++ leetcode_easy/0120.js
@@ -30,3 +30,17 @@ var isValid = function(s) {
    
     return !result.length
 };
+
+// **************************************
+
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+// Specifically, ans is the concatenation of two nums arrays.
+
+// Return the array ans.
+
+// ================= 풀이 ================
+
+var getConcatenation = function(nums) {
+    return [...nums, ...nums]
+};
