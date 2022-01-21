579447fe678fa9e3926efbc0791a4d7717f35ec5
sangki2070 <devsangki@gmail.com>
2022-01-21 23:23:53 +0900

    0121 algorithm 2

--- leetcode_easy/0121.js
+++ leetcode_easy/0121.js
@@ -13,3 +13,16 @@ var runningSum = function(nums) {
     }
     return answer
 };
+
+// ****************************
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+// ============ 풀이 ==============
+
+var defangIPaddr = function(address) {
+    
+    return address.replace(/\./g, '[.]')
+};
