40198a84eda26ec14a08f6d43b4b8298773a72dd
sangki2070 <devsangki@gmail.com>
2022-01-21 23:36:35 +0900

    0121 algorithm 3

--- leetcode_easy/0121.js
+++ leetcode_easy/0121.js
@@ -26,3 +26,24 @@ var defangIPaddr = function(address) {
     
     return address.replace(/\./g, '[.]')
 };
+
+// ****************************
+
+
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+// ============ 풀이 ==============
+
+var maximumWealth = function(accounts) {
+    
+    for(let i = 0; i < accounts.length; i ++){
+        console.log(accounts[i])
+        accounts[i] = accounts[i].reduce((acc,cur) => acc += cur)
+    }
+    
+    accounts.sort((a,b) => b-a)
+    
+    return accounts[0]
+};
