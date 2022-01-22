aebf86f66afdcd2a3f052901a40ab95c3d06246d
sangki2070 <devsangki@gmail.com>
2022-01-22 18:22:44 +0900

    0122 algorithm 4

--- leetcode_easy/0122.js
+++ leetcode_easy/0122.js
@@ -16,3 +16,28 @@ var shuffle = function(nums, n) {
     }
     return result
 };
+
+// *********************************
+
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of candies.
+
+// ============= 풀이 ================
+
+var kidsWithCandies = function(candies, extraCandies) {
+    let kids = Math.max(...candies)
+
+    let result = []
+    for(let i = 0; i < candies.length; i++){
+        if(candies[i] + extraCandies < kids){
+            result.push(false)
+        } else {
+            result.push(true)
+        }
+    }
+    
+    return result
+};
