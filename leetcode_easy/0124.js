0360fef7f856904aed77558b643842a3c0dbf623
sangki2070 <devsangki@gmail.com>
2022-01-24 23:31:40 +0900

    0124 algorithm 6

--- leetcode_easy/0124.js
+++ leetcode_easy/0124.js
@@ -126,3 +126,28 @@ var decompressRLElist = function(nums) {
     
     return result.flat()
 };
+
+// *************************************
+
+// Given an integer num, return the number of steps to reduce it to zero.
+
+// In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+// ============= 풀이 ================
+
+var numberOfSteps = function(num) {
+    
+    let count = 0
+    
+    while(num > 0){
+        if(num % 2 === 0){
+            num /= 2
+            count ++
+        } else {
+            num -= 1
+            count ++
+        }
+    }
+    
+    return count
+};
