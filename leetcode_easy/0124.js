b56e925b4361d985203b310125f55a4350df664e
sangki2070 <devsangki@gmail.com>
2022-01-24 16:25:49 +0900

    0124 algorithm 2

--- leetcode_easy/0124.js
+++ leetcode_easy/0124.js
@@ -15,3 +15,27 @@ var numJewelsInStones = function(jewels, stones) {
     
     return result
 };
+
+// *************************************
+
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+// Return the answer in an array.
+
+// ============= 풀이 ================
+
+var smallerNumbersThanCurrent = function(nums) {
+    let result = []
+    
+    for(let i = 0; i < nums.length; i ++){
+        result[i] = 0
+       for(let j =  1; j <nums.length; j++){
+           if(nums[0] > nums[j]){
+               result[i] += 1
+           }
+       }
+        nums.push(nums[0])
+        nums.shift()
+    }
+    return result
+};
