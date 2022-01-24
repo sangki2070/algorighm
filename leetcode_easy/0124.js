c7fd963cc54757b7bb01aeae1f04e63c92ead4b0
sangki2070 <devsangki@gmail.com>
2022-01-24 19:04:29 +0900

    0124 algorithm 5

--- leetcode_easy/0124.js
+++ leetcode_easy/0124.js
@@ -104,3 +104,25 @@ var restoreString = function(s, indices) {
     }
     return result.join("")
 };
+
+// *************************************
+
+
+// We are given a list nums of integers representing a list compressed with run-length encoding.
+
+// Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+// Return the decompressed list.
+
+// ============= 풀이 ================
+
+
+var decompressRLElist = function(nums) {
+    let result = []
+    
+    for(let i = 0; i < nums.length; i += 2){
+        result.push(Array(nums[i]).fill(nums[i + 1]))
+    }
+    
+    return result.flat()
+};
