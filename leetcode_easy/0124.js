0e60b1630605762d26b4e7e5a2e3a810a8578488
sangki2070 <devsangki@gmail.com>
2022-01-24 17:22:12 +0900

    0124 algorithm 4

--- leetcode_easy/0124.js
+++ leetcode_easy/0124.js
@@ -60,3 +60,25 @@ var subtractProductAndSum = function(n) {
     }
     return x - sum
 };
+
+// *************************************
+
+
+// There is a hidden integer array arr that consists of n non-negative integers.
+
+// It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+
+// You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+
+// Return the original array arr. It can be proved that the answer exists and is unique.
+
+// ============= 풀이 ================
+
+var decode = function(encoded, first) {
+    let origin = [first]
+    
+    for(let i = 0; i < encoded.length; i ++){
+       origin.push(origin[i] ^ encoded[i])
+    }
+    return origin
+};
