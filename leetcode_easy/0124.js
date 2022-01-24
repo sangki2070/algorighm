87b1a8672bf71061556fd3e5e2cbd0f3c4d33d18
sangki2070 <devsangki@gmail.com>
2022-01-24 15:12:21 +0900

    0124 algorithm

---
+++ leetcode_easy/0124.js
@@ -0,0 +1,17 @@
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// ============= 풀이 ================
+
+var numJewelsInStones = function(jewels, stones) {
+    let result = 0
+    
+    for(let i = 0; i < stones.length; i ++){
+        if(jewels.split("").includes(stones[i])){
+            result += 1
+        }
+    }
+    
+    return result
+};
