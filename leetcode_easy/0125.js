7d9ccec9f1804b6af63d509ffb40e23891616a50
sangki2070 <devsangki@gmail.com>
2022-01-25 17:53:58 +0900

    0125 algorithm 5

--- leetcode_easy/0125.js
+++ leetcode_easy/0125.js
@@ -102,3 +102,21 @@ var interpret = function(command) {
     }
     return count
 };
+
+// *******************************
+
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+// A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+// For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+// Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+
+ // ============= 풀이 ================
+
+ var sortSentence = function(s) {
+    let arr = s.split(" ").sort((a,b) => a.slice(a.length-1,a.length) - b.slice(b.length-1,b.length))
+    
+    return arr.join(" ").replace(/[0-9]/g, "")
+};
