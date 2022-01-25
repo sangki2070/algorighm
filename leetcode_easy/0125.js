3257f10f06547965b65d6491b93be16af9c7def0
sangki2070 <devsangki@gmail.com>
2022-01-25 15:36:48 +0900

    0125 algorithm2

--- leetcode_easy/0125.js
+++ leetcode_easy/0125.js
@@ -18,3 +18,16 @@ var createTargetArray = function(nums, index) {
     
     return result
 };
+
+// *******************************
+
+// You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+// Given the string command, return the Goal Parser's interpretation of command.
+
+// ============= 풀이 ================
+
+var interpret = function(command) {
+    let result = command.replace(/\(\)/g,"o",).split("")
+    return result.filter((el) => el !== "(" && el!== ")").join("")
+};
